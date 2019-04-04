054a6bae66bcbec356c249fb72076ac423ab4690
Luis Cabrita Vázquez <41645693+cabritavazquez@users.noreply.github.com>
2019-04-04 17:49:27 +0200

    Update PasoPorDireccion.c

--- PasoPorDireccion.c
+++ PasoPorDireccion.c
@@ -14,7 +14,7 @@ int main(){
 	*p = 40; // El contenido al que apunta p = 40
 	p = numbers; // Pongo a p apuntando a numbers (posición 0 del array)
 	*(p+4) = 50; // El contenido al que apunta p -> numbers[0]
-				 // le sumo 4 posiciones p-> numbers[4] y le meto un 
+		     // le sumo 4 posiciones p-> numbers[4] y le meto un 
 	
 	for (int n = 0; n < 5; n++){
 		std:: cout << numbers[n] << std:: endl ;
